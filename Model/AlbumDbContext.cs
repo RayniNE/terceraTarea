6ceb820ebdc682bb780d0c66888d26df535779a1
Rayni Nuñez <timetopwm@gmail.com>
2020-02-09 19:31:47 -0400

    version1

---
+++ Model/AlbumDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class AlbumDbContext : DbContext
+    {
+        public AlbumDbContext(DbContextOptions<AlbumDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Album { get; set; }
+    }
+}
